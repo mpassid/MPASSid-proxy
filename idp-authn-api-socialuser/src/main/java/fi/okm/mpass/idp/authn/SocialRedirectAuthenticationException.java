8851fd217954f35f2f10fba5593e2dc9a0d58b27
jalauros <janne.lauros@gmail.com>
2015-10-13 14:19:20 +0300

    suppress warning serial added

--- idp-authn-api-socialuser/src/main/java/fi/okm/mpass/idp/authn/SocialRedirectAuthenticationException.java
+++ idp-authn-api-socialuser/src/main/java/fi/okm/mpass/idp/authn/SocialRedirectAuthenticationException.java
@@ -27,6 +27,7 @@ import javax.annotation.Nonnull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@SuppressWarnings("serial")
 public class SocialRedirectAuthenticationException extends Exception {
 
     /* Authentication event id */
