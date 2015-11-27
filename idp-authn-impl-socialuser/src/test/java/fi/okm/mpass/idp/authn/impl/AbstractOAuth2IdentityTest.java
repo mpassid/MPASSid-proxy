1312997514f1255a980e816cb5bd45e4716b0a08
jalauros <janne.lauros@gmail.com>
2015-11-27 09:37:30 +0200

    claim parsing tests added

--- idp-authn-impl-socialuser/src/test/java/fi/okm/mpass/idp/authn/impl/AbstractOAuth2IdentityTest.java
+++ idp-authn-impl-socialuser/src/test/java/fi/okm/mpass/idp/authn/impl/AbstractOAuth2IdentityTest.java
@@ -27,8 +27,15 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import javax.annotation.Nonnull;
+import javax.json.JsonArray;
 import javax.security.auth.Subject;
 
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -37,6 +44,11 @@ import fi.okm.mpass.idp.authn.principal.SocialUserPrincipal;
 
 public class AbstractOAuth2IdentityTest {
 
+    /** Class logger. */
+    @Nonnull
+    private final Logger log = LoggerFactory
+            .getLogger(AbstractOAuth2IdentityTest.class);
+    
     private AbstractOAuth2Identity abstractOAuth2Identity;
     private boolean prin1Match;
     private boolean prin2Match;
@@ -50,6 +62,7 @@ public class AbstractOAuth2IdentityTest {
         oauth2PrincipalsDefaults.put("principal2", "value2");
         oauth2PrincipalsDefaults.put("principal3", "value3");
         abstractOAuth2Identity.setPrincipalsDefaults(oauth2PrincipalsDefaults);
+        
     }
 
     private void performMatch(Subject subject) {
@@ -104,5 +117,68 @@ public class AbstractOAuth2IdentityTest {
         Assert.assertEquals(prin2Match, false);
         Assert.assertEquals(prin3Match, true);
     }
+    
+    @Test
+    public void testParsingPrincipalsFromSubject() throws Exception {
+        
+        Map<String, String> oauth2ClaimsPrincipals = new HashMap<String, String>();
+        oauth2ClaimsPrincipals.put("sub", "userId");
+        oauth2ClaimsPrincipals.put("urn:oid:1.2.246.22", "special");
+        oauth2ClaimsPrincipals.put("urn:oid:1.2.246.XX", "special2");
+        abstractOAuth2Identity.setClaimsPrincipals(oauth2ClaimsPrincipals);
+        
+        Map<String, String> oauth2CustomClaimTypes = new HashMap<String, String>();
+        oauth2CustomClaimTypes.put("sub", "notsupportedtype");
+        oauth2CustomClaimTypes.put("urn:oid:1.2.246.22", "jsonarray");
+        oauth2CustomClaimTypes.put("urn:oid:1.2.246.XX", "jsonarray");
+        abstractOAuth2Identity.setCustomClaimsTypes(oauth2CustomClaimTypes);
+        
+        
+        JSONObject json=new JSONObject();
+        JSONArray jsonArray=new JSONArray();
+        jsonArray.add("010191-123A");
+        JSONArray jsonArray2=new JSONArray();
+        jsonArray2.add("Tammi Tauno Matias");
+        jsonArray2.add("Matiasson");
+        
+        json.put("sub", "CN=test,OU=Luottamusverkosto,CN=Ubilogin,DC=oidc,DC=ubidemo,DC=com");
+        json.put("aud","zjaj93fcf5os1zbjt2pe5jrubb4xkeqw3lgm");
+        json.put("urn:oid:1.2.246.22", jsonArray);
+        json.put("urn:oid:1.2.246.XX", jsonArray2);
+        json.put("iat", 354739);
+        
+        Subject subject = new Subject();
+        abstractOAuth2Identity.parsePrincipalsFromClaims(subject, json);
+        //5 claims, 3 mapped and one of the 3 is set as userprincipal
+        //Assert.assertEquals(4,subject.getPrincipals().size());
+        
+        boolean bFind1=false;
+        boolean bFind2=false;
+        boolean bFind3=false;
+        boolean bFind4=false;
+        final Set<SocialUserPrincipal> principals = subject
+                .getPrincipals(SocialUserPrincipal.class);
+        for (SocialUserPrincipal sprin : principals) {
+            if ("userId".equals(sprin.getType()) && "CN=test,OU=Luottamusverkosto,CN=Ubilogin,DC=oidc,DC=ubidemo,DC=com".equals(sprin.getValue())){
+                bFind1=true;
+            }
+            if ("special".equals(sprin.getType()) && "010191-123A".equals(sprin.getValue())){
+                bFind2=true; 
+            }
+            if ("special2".equals(sprin.getType()) && "Tammi Tauno Matias".equals(sprin.getValue())){
+                bFind3=true; 
+            }
+            if ("special2".equals(sprin.getType()) && "Matiasson".equals(sprin.getValue())){
+                bFind4=true; 
+            }
+        }
+        Assert.assertTrue(bFind1);
+        Assert.assertTrue(bFind2);
+        Assert.assertTrue(bFind3);
+        Assert.assertTrue(bFind4);
+        
+        
+    }
+    
 
 }
