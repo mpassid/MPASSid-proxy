c5cfe4d96e3865b098109e2b506ec572f580ec15
Henri Mikkonen <henri.mikkonen@iki.fi>
2016-03-17 15:18:31 +0200

    Fixed typo in populateHeaderPrincipals

--- idp-authn-impl-shibsp/src/main/java/fi/okm/mpass/shibboleth/authn/impl/ValidateShibbolethAuthentication.java
+++ idp-authn-impl-shibsp/src/main/java/fi/okm/mpass/shibboleth/authn/impl/ValidateShibbolethAuthentication.java
@@ -218,7 +218,7 @@ public class ValidateShibbolethAuthentication extends AbstractValidationAction {
      * @return The set containing all HTTP headers.
      */
     protected Set<Principal> populateHeaderPrincipals() {
-        return populatePrincipals(shibbolethContext.getHeaders(), false);
+        return populatePrincipals(shibbolethContext.getHeaders(), true);
     }
     
     /**
