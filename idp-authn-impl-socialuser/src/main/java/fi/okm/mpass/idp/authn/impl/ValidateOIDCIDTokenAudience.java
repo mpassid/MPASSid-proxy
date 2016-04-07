c79adbd7187fe487022e15c4b3932bf9d1edffce
jalauros <janne.lauros@gmail.com>
2016-04-07 12:46:00 +0300

    validates now audience

--- idp-authn-impl-socialuser/src/main/java/fi/okm/mpass/idp/authn/impl/ValidateOIDCIDTokenAudience.java
+++ idp-authn-impl-socialuser/src/main/java/fi/okm/mpass/idp/authn/impl/ValidateOIDCIDTokenAudience.java
@@ -23,14 +23,19 @@
 
 package fi.okm.mpass.idp.authn.impl;
 
+import java.text.ParseException;
+
 import javax.annotation.Nonnull;
 
 import net.shibboleth.idp.authn.AbstractAuthenticationAction;
+import net.shibboleth.idp.authn.AuthnEventIds;
 import net.shibboleth.idp.authn.context.AuthenticationContext;
 
+import org.opensaml.profile.action.ActionSupport;
 import org.opensaml.profile.context.ProfileRequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 /**
  * An action that verifies Audience of ID Token.
@@ -58,6 +63,48 @@ public class ValidateOIDCIDTokenAudience extends AbstractAuthenticationAction {
             @Nonnull final ProfileRequestContext profileRequestContext,
             @Nonnull final AuthenticationContext authenticationContext) {
         log.trace("Entering");
+
+        final SocialUserOpenIdConnectContext suCtx = authenticationContext
+                .getSubcontext(SocialUserOpenIdConnectContext.class, true);
+        if (suCtx == null) {
+            log.error("{} Not able to find su oidc context", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext,
+                    AuthnEventIds.NO_CREDENTIALS);
+            log.trace("Leaving");
+            return;
+        }
+
+        // The Client MUST validate that the aud (audience) Claim contains
+        // its client_id value registered at the Issuer identified by the
+        // iss (issuer) Claim as an audience. The aud (audience) Claim MAY
+        // contain an array with more than one element. The ID Token MUST be
+        // rejected if the ID Token does not list the Client as a valid
+        // audience, or if it contains additional audiences not trusted by
+        // the Client.
+        try {
+            if (!suCtx
+                    .getOidcTokenResponse()
+                    .getOIDCTokens()
+                    .getIDToken()
+                    .getJWTClaimsSet()
+                    .getAudience()
+                    .contains(
+                            suCtx.getOpenIdConnectInformation().getClientId()
+                                    .getValue())) {
+                log.error("{} client is not the intended audience",
+                        getLogPrefix());
+                ActionSupport.buildEvent(profileRequestContext,
+                        AuthnEventIds.NO_CREDENTIALS);
+                log.trace("Leaving");
+                return;
+            }
+        } catch (ParseException e) {
+            log.error("{} Error parsing id token", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext,
+                    AuthnEventIds.NO_CREDENTIALS);
+            log.trace("Leaving");
+            return;
+        }
         log.trace("Leaving");
         return;
     }
