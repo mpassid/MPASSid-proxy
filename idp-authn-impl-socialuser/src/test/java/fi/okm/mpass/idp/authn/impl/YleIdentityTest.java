250dcfb571229ef0493a53deb364170ba0fb1b81
Henri Mikkonen <henri.mikkonen@iki.fi>
2016-05-17 16:17:50 +0300

    Refactoring

--- idp-authn-impl-socialuser/src/test/java/fi/okm/mpass/idp/authn/impl/YleIdentityTest.java
+++ idp-authn-impl-socialuser/src/test/java/fi/okm/mpass/idp/authn/impl/YleIdentityTest.java
@@ -23,9 +23,6 @@
 
 package fi.okm.mpass.idp.authn.impl;
 
-import java.io.StringReader;
-import java.net.InetSocketAddress;
-import java.net.SocketAddress;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,14 +30,6 @@ import javax.annotation.Nonnull;
 import javax.security.auth.Subject;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.io.IOUtils;
-import org.simpleframework.http.Request;
-import org.simpleframework.http.Response;
-import org.simpleframework.http.core.Container;
-import org.simpleframework.http.core.ContainerSocketProcessor;
-import org.simpleframework.transport.SocketProcessor;
-import org.simpleframework.transport.connect.Connection;
-import org.simpleframework.transport.connect.SocketConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -48,14 +37,12 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.nimbusds.oauth2.sdk.id.State;
-
 import fi.okm.mpass.idp.authn.SocialUserAuthenticationException;
 
 /**
  * Unit tests for {@link YleIdentity}.
  */
-public class YleIdentityTest {
+public class YleIdentityTest extends OAuth2IdentityTest {
     
     @Nonnull
     private final Logger log = LoggerFactory.getLogger(YleIdentityTest.class);
@@ -66,39 +53,14 @@ public class YleIdentityTest {
     /** Yle App Key. */
     private String appKey;
     
-    /** Client identifier. */
-    private String clientId;
-    
-    /** Client secret. */
-    private String clientSecret;
-    
-    /** The token endpoint for local testing. */
-    private String tokenEndpoint;
-    
-    /** The user info endpoint for local testing. */
-    private String userInfoEndpoint;
-    
-    /** The user claim key. */
-    private String userClaim;
-    
-    /** The error code. */
-    private String errorCode;
-    
-    /** The error description. */
-    private String errorDescription;
-    
     /**
      * Set up tests.
      */
     @BeforeMethod
     public void setUp() {
+        super.setUp();
         appId = "mockAppId";
         appKey = "mockAppKey";
-        clientId = "mockClientId";
-        clientSecret = "mockClientSecret";
-        final String urlPrefix = "http://localhost:" + SetOIDCInformationTest.CONTAINER_PORT;
-        tokenEndpoint = urlPrefix + "/token";
-        userInfoEndpoint = urlPrefix + "/userinfo";
         userClaim = "user_key";
         errorCode = "access_denied";
         errorDescription = "mock description";
@@ -142,125 +104,31 @@ public class YleIdentityTest {
         Assert.assertTrue(redirectUrl.contains("client_id=" + clientId));
     }
 
-    /**
-     * Runs getSubject with empty {@link HttpServletRequest}.
-     * @throws Exception
-     */
+    /** {@inheritDoc} */
     @Test public void testSubjectEmptyRequest() throws Exception {
-        final YleIdentity yleId = initYleIdentity();
-        Assert.assertNull(yleId.getSubject(new MockHttpServletRequest()));
+        super.testSubjectEmptyRequest(initYleIdentity());
     }
 
-    /**
-     * Runs getSubject with error token response.
-     * @throws Exception
-     */
+    /** {@inheritDoc} */
     @Test public void testSubjectErrorToken() throws Exception {
-        final YleIdentity yleId = initYleIdentity();
-        final String urlPrefix = "http://localhost:" + SetOIDCInformationTest.CONTAINER_PORT;
-        final String tokenEndpoint = urlPrefix + "/errorToken";
-        yleId.setTokenEndpoint(tokenEndpoint);
-        yleId.setUserinfoEndpoint(userInfoEndpoint);
-        final MockHttpServletRequest httpRequest = initHttpServletRequest();
-        String exception = null;
-        try {
-            executeGetSubjectWithServer(yleId, httpRequest);
-        } catch (SocialUserAuthenticationException e) {
-            exception = e.getMessage();
-        }
-        Assert.assertNotNull(exception);
-        Assert.assertTrue(exception.startsWith(errorCode));
-        Assert.assertTrue(exception.contains(errorDescription));
+        super.testSubjectErrorToken(initYleIdentity());
     }
 
-    /**
-     * Runs getSubject with unparseable token response.
-     * @throws Exception
-     */
+    /** {@inheritDoc} */
     @Test public void testSubjectUnparseableToken() throws Exception {
-        final YleIdentity yleId = initYleIdentity();
-        final MockHttpServletRequest httpRequest = initHttpServletRequest();
-        String exception = null;
-        try {
-            executeGetSubjectWithServer(yleId, httpRequest, true, false);
-        } catch (SocialUserAuthenticationException e) {
-            exception = e.getMessage();
-        }
-        Assert.assertNotNull(exception);
+        super.testSubjectUnparseableToken(initYleIdentity());
     }
 
-    /**
-     * Runs getSubject with unparseable user info response.
-     * @throws Exception
-     */
+    /** {@inheritDoc} */
     @Test public void testSubjectUnparseableUserInfo() throws Exception {
-        final YleIdentity yleId = initYleIdentity();
-        final MockHttpServletRequest httpRequest = initHttpServletRequest();
-        String exception = null;
-        try {
-            executeGetSubjectWithServer(yleId, httpRequest, false, true);
-        } catch (SocialUserAuthenticationException e) {
-            exception = e.getMessage();
-        }
-        Assert.assertNotNull(exception);
+        super.testSubjectUnparseableUserInfo(initYleIdentity());
     }    
     /**
-     * Runs getSubject with prerequisites fulfilled.
-     * @throws Exception
-     */
+    /** {@inheritDoc} */
     @Test public void testSubjectSuccess() throws Exception {
-        final YleIdentity yleId = initYleIdentity();
-        final MockHttpServletRequest httpRequest = initHttpServletRequest();
-        final Subject subject = executeGetSubjectWithServer(yleId, httpRequest);
-        Assert.assertNotNull(subject);
-        Assert.assertEquals(subject.getPrincipals().iterator().next().getName(), "mockUser");
+        super.testSubjectSuccess(initYleIdentity());
     }
     
-    /**
-     * Initializes a servlet request.
-     * @return
-     */
-    protected MockHttpServletRequest initHttpServletRequest() {
-        final MockHttpServletRequest httpRequest = new MockHttpServletRequest();
-        httpRequest.setQueryString("code=mockCode&state=mockState");
-        httpRequest.getSession(true).setAttribute(AbstractOAuth2Identity.SESSION_ATTR_STATE, new State("mockState"));
-        return httpRequest;
-    }
-    
-    /**
-     * Executes the getSubject method with simple container running.
-     * @param yleId
-     * @param httpRequest
-     * @return
-     * @throws Exception
-     */
-    protected Subject executeGetSubjectWithServer(final YleIdentity yleId, final HttpServletRequest httpRequest) throws Exception {
-        return executeGetSubjectWithServer(yleId, httpRequest, false, false);
-    }
-    
-    /**
-     * Executes the getSubject method with simple container running.
-     * @param yleId
-     * @param httpRequest
-     * @return
-     * @throws Exception
-     */
-    protected Subject executeGetSubjectWithServer(final YleIdentity yleId, final HttpServletRequest httpRequest, 
-            final boolean unparseableToken, final boolean unparseableUserInfo) throws Exception {
-        final Container container = new SimpleContainer(unparseableToken, unparseableUserInfo);
-        final SocketProcessor server = new ContainerSocketProcessor(container);
-        final Connection connection = new SocketConnection(server);
-        final SocketAddress address = new InetSocketAddress(SetOIDCInformationTest.CONTAINER_PORT);
-        connection.connect(address);
-        try {
-            return yleId.getSubject(httpRequest);
-        } catch (Exception e) {
-            throw e;
-        } finally {
-            connection.close();
-        }
-    }
-
     /**
      * Initializes {@link YleIdentity} with default settings.
      * @return
@@ -276,54 +144,7 @@ public class YleIdentityTest {
         final Map<String, String> claims = new HashMap<>();
         claims.put(userClaim, "userId");
         yleId.setClaimsPrincipals(claims);
+        yleId.init();
         return yleId;
-    }
-    
-    /**
-     * Simple container implementation.
-     */
-    class SimpleContainer implements Container {
-        
-        final boolean unparseableToken;
-        
-        final boolean unparseableUserInfo;
-        
-        /**
-         * Constructor.
-         */
-        public SimpleContainer(final boolean throwToken, final boolean throwUserInfo) {
-            unparseableToken = throwToken;
-            unparseableUserInfo = throwUserInfo;
-        }
-
-        @Override
-        /** {@inheritDoc} */
-        public void handle(Request request, Response response) {
-            log.trace("Server got request for {}", request.getTarget());
-            try {
-                response.setContentType("application/json");
-                String output = "";
-                if (request.getTarget().contains("/token")) {
-                    if (unparseableToken) {
-                        output = "{ unparseable }";
-                    } else {
-                        output = "{ \"access_token\":\"2YotnFZFEjr1zCsicMWpAA\", \"token_type\":\"Bearer\", \"expires_in\":3600 }";
-                    }
-                } else if (request.getTarget().contains("/userinfo")) {
-                    if (unparseableUserInfo) {
-                        output = "{ unparseable }";
-                    } else {
-                        output = "{ \"" + userClaim + "\":\"mockUser\" }";
-                    }
-                } else if (request.getTarget().contains("/errorToken")) {
-                    output = "{ \"error\":\"" + errorCode + "\", \"error_description\":\"" + errorDescription + "\" }";
-                    response.setCode(500);
-                }
-                IOUtils.copy(new StringReader(output), response.getOutputStream());
-                response.getOutputStream().close();
-             } catch(Exception e) {
-                 log.error("Container-side exception ", e);
-             }  
-        }
     }
 }
